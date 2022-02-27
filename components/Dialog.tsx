7731bceb95816dbb19af310b84c2f2d02d248bbb
secondfolder <secondfolder@gmail.com>
2022-02-27 18:40:59 +1100

    Style overlay in Dialog component

--- components/Dialog.tsx
+++ components/Dialog.tsx
@@ -22,6 +22,7 @@ const Dialog = ({
             contentLabel="Edit Training Text Dialog"
             onRequestClose={onRequestClose}
             className={cx(style, className)}
+            overlayClassName={overlayStyle}
         >
             <CloseButton className="closeButton" onClick={onRequestClose} />
             {children}
@@ -36,9 +37,11 @@ const style = css`
     top: 50%;
     transform: translate(-50%,-50%);
     width: min(500px, 80%);
-    height: min(300px, 80%);
-    border: 1px solid #ccc;
-    background: #fff;
+    height: max-content;
+    min-height: 300px;
+    max-height: 80%;
+    border: 1px solid var(--colour-text-main);
+    background: var(--colour-background);
     overflow: auto;
     -webkit-overflow-scrolling: touch;
     border-radius: 4px;
@@ -54,4 +57,13 @@ const style = css`
         top: 0.2em;
         right: 0.2em;
     }
+`
+
+const overlayStyle = css`
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background-color: var(--colour-background-tone-translucent);
 `
